28446f50946c249e8a05392dab7022bb9b0f8e02
Kevin REMY <kevin.remy@dataiku.com>
2023-11-21 12:02:21 +0100

    Bump version 12.3.1 (#5)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import setuptools
 long_description = (open('README.md').read() + '\n\n' +
                     open('HISTORY.txt').read())
 
-VERSION = "12.2.3"
+VERSION = "12.3.1"
 
 setuptools.setup(
     name='dataiku-scoring',
