f2ec3fdb80f2383f203c34d4c1872bbd52ff2e6f
Kevin REMY <kevin.remy@dataiku.com>
2024-01-23 16:16:45 +0100

    Prepare release (#9)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import setuptools
 long_description = (open('README.md').read() + '\n\n' +
                     open('HISTORY.txt').read())
 
-VERSION = "12.4.2"
+VERSION = "12.5.0"
 
 setuptools.setup(
     name='dataiku-scoring',
