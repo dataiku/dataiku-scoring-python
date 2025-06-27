d46964ac89e16b7ac47bc3b32813caeec462bd5e
Clément Stenac <clement.stenac@dataiku.com>
2025-06-27 18:34:33 +0200

    Version update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import setuptools
 long_description = (open('README.md').read() + '\n\n' +
                     open('HISTORY.txt').read())
 
-VERSION = "13.5.5"
+VERSION = "14.0.0"
 
 setuptools.setup(
     name='dataiku-scoring',
