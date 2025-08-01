1c29ef222264433ae3c2f66398242918d867065a
Kevin REMY <kevin.remy@dataiku.com>
2025-08-01 16:35:51 +0200

    14.0.2 - Publish on PyPI (#44)
    * Sync sources with main repository
    
    * Release metadata

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import setuptools
 long_description = (open('README.md').read() + '\n\n' +
                     open('HISTORY.txt').read())
 
-VERSION = "14.0.1"
+VERSION = "14.0.2"
 
 setuptools.setup(
     name='dataiku-scoring',
