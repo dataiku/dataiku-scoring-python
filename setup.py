1320a5875fad6f6d2884170e86985fe893154d36
Kevin REMY <kevin.remy@dataiku.com>
2025-12-01 11:19:40 +0100

    Publish 14.2.3 on PyPI (#53)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import setuptools
 long_description = (open('README.md').read() + '\n\n' +
                     open('HISTORY.txt').read())
 
-VERSION = "14.2.2"
+VERSION = "14.2.3"
 
 setuptools.setup(
     name='dataiku-scoring',
