796ad7b59287ec5f0537e55fe3470092d25de632
Kevin REMY <kevin.remy@dataiku.com>
2026-03-06 10:01:02 +0100

    Publish 14.4.2 on PyPI (#60)
    * Release metadata
    * Remove duplicated entry

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import setuptools
 long_description = (open('README.md').read() + '\n\n' +
                     open('HISTORY.txt').read())
 
-VERSION = "14.4.1"
+VERSION = "14.4.2"
 
 setuptools.setup(
     name='dataiku-scoring',
