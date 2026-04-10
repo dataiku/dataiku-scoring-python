11526e9c7e7cd7400ac16e90b6532f5962e6879b
Kevin REMY <kevin.remy@dataiku.com>
2026-04-10 08:42:33 +0200

    14.4.4 - Publish on PyPI (#62)
    * Release metadata
    
    * Update HISTORY.txt
    
    Update release date

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import setuptools
 long_description = (open('README.md').read() + '\n\n' +
                     open('HISTORY.txt').read())
 
-VERSION = "14.4.3"
+VERSION = "14.4.4"
 
 setuptools.setup(
     name='dataiku-scoring',
