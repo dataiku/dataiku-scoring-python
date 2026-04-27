ed5da6b41a9ed5eef7c2772f9ead7e1b6d155ea2
Kevin REMY <kevanescence@hotmail.fr>
2026-04-27 09:51:05 +0200

    Release metadata

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import setuptools
 long_description = (open('README.md').read() + '\n\n' +
                     open('HISTORY.txt').read())
 
-VERSION = "14.5.0"
+VERSION = "14.5.1"
 
 setuptools.setup(
     name='dataiku-scoring',
